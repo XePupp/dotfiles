f8c39a23bde0c267d8c195904bfcb784d87738c5
XePup <110104965+XePupp@users.noreply.github.com>
2022-08-11 17:39:13 +1000

    Create arch.sh

---
+++ installscripts/arch.sh
@@ -0,0 +1,16 @@
+# Fetching Packages
+sudo pacman -S bspwm sxhkd polybar xorg xorg-xinit alacritty neovim neofetch git flatpak rofi flameshot
+flatpak install org.mozilla.firefox -y
+flatpak install org.gimp.GIMP -y 
+flatpak install com.github.tchx84.Flatseal -y
+flatpak install io.mpv.Mpv -y
+
+
+
+git clone https://github.com/xepupp/dotfiles
+mkdir ~/.config/
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+mkdir ~/.config/polybar
+mkdir ~/.config/rofi
+mkdir ~/.config/
